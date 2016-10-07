5abb0bbaa2a3932da4ea6bec48e3b3b8b2dd8d04
DWIGHT L MARTIN <dmartin54@student.gsu.edu>
2016-10-07 00:05:56 -0400

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,25 +1,3 @@
-const bw = 400;
-const bh = 400;
-const p = 10;
-
-const canvas = document.getElementById('tetris');
-const context = canvas.getContext('2d');
-function drawBoard(){
-for (var x = 0; x <= bw; x += 40) {
-    context.moveTo(0.5 + x + p, p);
-    context.lineTo(0.5 + x + p, bh + p);
-}
-
-for (var x = 0; x <= bh; x += 40) {
-    context.moveTo(p, 0.5 + x + p);
-    context.lineTo(bw + p, 0.5 + x + p);
-}
-
-context.strokeStyle = "black";
-context.stroke();
-}
-
-drawBoard();
 
 
 /*access to canvas and get context out */
